fe680c0efa5e11b80fd6b172e031bbc3e1ff18ea
Juuso Mikkonen <hello@juusomikkonen.com>
2018-12-09 11:39:58 +0200

    Solve the last part of day 3

--- 3/ab.hs
+++ 3/ab.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.List
 import Text.Regex
+import qualified Data.Map as M
 
 type Id = Int
 type Coordinate = (Int, Int)
@@ -19,6 +20,15 @@ areaToCoords (_,(x, y),(w, h)) = (,) <$> xs <*> ys
                                   where xs = [x..(x+w-1)]
                                         ys = [y..(y+h-1)]
 
+
+overlaps :: M.Map Coordinate Int -> Area -> Bool
+overlaps m a = foldl (\b c -> b || 1 < M.findWithDefault 0 c m) False $ areaToCoords a
+
+findNonOverlapping :: M.Map Coordinate Int -> [Area] -> Area
+findNonOverlapping m (a:rest)
+  | overlaps m a = findNonOverlapping m rest
+  | otherwise = a
+
 main = do
   content <- readFile "input.txt"
   let l = lines content
@@ -26,3 +36,5 @@ main = do
   let covered = foldl (\l -> \a -> l ++ areaToCoords a) [] areas
   let groups = groupBy (==) (sort covered)
   print $ length $ filter ((<=) 2) $ map length $ groups
+  let coordMap = M.fromList $ map (\(x:xs) -> (x, 1 + length xs)) groups
+  print $ findNonOverlapping coordMap areas
